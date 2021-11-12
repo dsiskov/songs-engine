3ef9456ad58aaa364ac81a7a677187753ce56de7
dsiskov <dragansiskov@gmail.com>
2021-11-12 20:09:36 +0100

    client(auth): Introduce api usage logic

--- client/src/configuration/routes.js
+++ client/src/configuration/routes.js
@@ -5,4 +5,8 @@ export default [
     component: require('presenters/pages/radio-tracker').default,
     exact: true,
   },
+  {
+    path: '/login',
+    component: require('presenters/pages/login').default,
+  },
 ]
---
+++ client/src/presenters/pages/login/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import api from 'api'
+
+const mockUser = {
+  username: 'user1',
+  password: '1resu',
+}
+
+const Login = (props) => {
+  const onSignUp = async () => {
+    const res = await api.post('auth/signup', mockUser)
+    console.log(res.data)
+  }
+
+  return (
+    <div>
+      <input type='button' value={'sign-up'} id='name' onClick={onSignUp} />
+    </div>
+  )
+}
+
+export default Login
