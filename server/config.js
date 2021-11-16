e7eeb4997c10b38805893cd9932e625f92c26733
dsiskov <dragansiskov@gmail.com>
2021-11-16 13:12:14 +0100

    server(improvements): Remove obsolete properties

--- server/config.js
+++ server/config.js
@@ -1,11 +1,8 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
-const { MONGO_URL, USE_TEST_DB, TEST_DB_URL, ...env } = process.env
-
-if (USE_TEST_DB && !TEST_DB_URL) throw 'Please define TEST_DB_URL'
+const { ...env } = process.env
 
 module.exports = {
   ...env,
-  MONGO_URL: USE_TEST_DB ? TEST_DB_URL : MONGO_URL,
 }
